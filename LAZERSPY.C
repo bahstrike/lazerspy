44914a012438de4023afe51bea76ea0441908bc1
bahstrike <strike_1337@hotmail.com>
2021-09-12 03:45:23 -0400

    +added initial thing stuff

--- LAZERSPY.C
+++ LAZERSPY.C
@@ -33,37 +33,53 @@ unsigned char bgColor = C_LIGHTCYAN;
 void main()
 {
 	short x, y;
+	thing* plr;
 
 	gfxinit();
 
 	cratesprite4x4 = createsprite(4, 4, _cratesprite4x4);
 	guy8x8 = createsprite(8, 8, _guy8x8);
 
-	pset(10,10,1);	//gfx[ 10 + 10*scrWidth ] = 1;
+
+	worldinit();
+
+	plr = creatething(
 
 
-	fillrect(0, SCRHEIGHT-30, SCRWIDTH, SCRHEIGHT, C_DARKGRAY);// TEST FILL
-	fillrect(20, SCRHEIGHT-30, SCRWIDTH-20, SCRHEIGHT-15, C_BLUE);// TEST FILL
-
-	for(x=10; x<300; x+=5)
-		for(y=10; y<200; y+=130)
-			drawsprite(cratesprite4x4, x, y);
-
-	drawsprite(guy8x8, 100, 100);
-
+	for(;;)
 	{
-		int ver = 420;
-		char sz[64];
+		float dt = 1.0f / 20.0f; // some standard 20fps dt
 
-		sprintf(sz, "lazerspy v%d.%d", (ver/100), (ver%100) );
+		if(kbhit())
+			break;
 
-		drawtext(1, 1, sz);
+		pset(10,10,1);	//gfx[ 10 + 10*scrWidth ] = 1;
 
+
+		fillrect(0, SCRHEIGHT-30, SCRWIDTH, SCRHEIGHT, C_DARKGRAY);// TEST FILL
+		fillrect(20, SCRHEIGHT-30, SCRWIDTH-20, SCRHEIGHT-15, C_BLUE);// TEST FILL
+
+		for(x=10; x<300; x+=5)
+			for(y=10; y<200; y+=130)
+				drawsprite(cratesprite4x4, x, y);
+
+		drawsprite(guy8x8, 100, 100);
+
+		{
+			int ver = 420;
+			char sz[64];
+
+			sprintf(sz, "lazerspy v%d.%d", (ver/100), (ver%100) );
+
+			drawtext(1, 1, sz);
+		}
+
+		worldprocess(dt);
 		gfxpageflip();
 	}
 
+//	waitforkey();
 
-	waitforkey();
-
+	worldshutdown();
 	gfxshutdown();
 }
---
+++ THING.C
@@ -0,0 +1,13 @@
+#include "thing.h"
+
+void thingreset(thing* t)
+{
+	if(t == 0)
+	   return;
+
+	t->x = 0.0f;
+	t->y = 0.0f;
+	t->vx = 0.0f;
+	t->vy = 0.0f;
+
+}
